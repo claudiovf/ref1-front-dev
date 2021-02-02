eb4c5b446a349c94fa200a8fafdc44467f244108
Claudio <claudiovf@hotmail.com>
2021-02-02 20:01:53 +1100

    initial commit. Dependencies initial config

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HeaderStyled, LogoStyled } from './style';
+
+
+const Header: React.FC = () => {
+
+
+  return (
+        <HeaderStyled> 
+            <LogoStyled>REF1</LogoStyled> 
+        </HeaderStyled>
+  );
+};
+
+export default Header;
