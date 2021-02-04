fa5c09167c91020fda866dc2574a43c7daad874d
Claudio <claudiovf@hotmail.com>
2021-02-04 20:23:41 +1100

    cors update

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,8 @@ import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/c
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({ 
-    uri: 'http://localhost:4000' 
+    uri: 'https://ref1-app-backend.herokuapp.com',
+    credentials: 'include',
   })
 });
 
