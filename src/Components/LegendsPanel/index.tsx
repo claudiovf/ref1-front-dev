1cbfb5d50a0430207f3ae65d87c60efd7cb8dd60
Claudio <claudiovf@hotmail.com>
2021-04-19 19:58:19 +1000

    init PWA

--- src/Components/LegendsPanel/index.tsx
+++ src/Components/LegendsPanel/index.tsx
@@ -1,72 +1,10 @@
 import React from 'react';
 import { Section, Title, Scroll } from '../LayoutComponents';
 import CurrentDriverCards from '../CurrentDriversPanel/CurrentDriverCards';
+import { legendsList } from '../../utils/currentInfo';
 
 
 const LegendsPanel: React.FC = () => {
-
-    const legendsList = [
-        {
-            driverId: "michael_schumacher",
-            givenName: "Michael",
-            familyName: "Schumacher"
-        },
-        {
-            driverId: "senna",
-            givenName: "Ayrton",
-            familyName: "Senna"
-        },
-        {
-            driverId: "prost",
-            givenName: "Alain",
-            familyName: "Prost"
-        },
-        {
-            driverId: "mansell",
-            givenName: "Nigel",
-            familyName: "Mansell"
-        },
-        {
-            driverId: "jack_brabham",
-            givenName: "Jack",
-            familyName: "Brabham"
-        },
-        {
-            driverId: "stewart",
-            givenName: "Jackie",
-            familyName: "Stewart"
-        },
-        {
-            driverId: "lauda",
-            givenName: "Niki",
-            familyName: "Lauda"
-        },
-        {
-            driverId: "moss",
-            givenName: "Stirling",
-            familyName: "Moss"
-        },
-        {
-            driverId: "piquet",
-            givenName: "Nelson",
-            familyName: "Piquet"
-        },
-        {
-            driverId: "fangio",
-            givenName: "Juan",
-            familyName: "Fangio"
-        },
-        {
-            driverId: "clark",
-            givenName: "Jim",
-            familyName: "Clark"
-        },
-        {
-            driverId: "emerson_fittipaldi",
-            givenName: "Emerson",
-            familyName: "Fittipaldi"
-        },
-    ];
 
     return (
         <React.Fragment>
